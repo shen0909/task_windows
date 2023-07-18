b5b0d5739ce250bd6719b265c7b92101641521e4
shen <1571817467@qq.com>
2023-07-18 20:39:53 +0800

    试试将本地的更新代码上传到远程仓库

---
+++ lib/key/key.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      home: HYHomePage(),
+    );
+  }
+}
+class HYHomePage extends StatelessWidget {
+  const HYHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("首页"),
+      ),
+      body: Center(
+        child: Text("Hello World!!"),
+      ),
+    );
+  }
+}
